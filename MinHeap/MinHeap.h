b7acf81bb12cc6d47bc0fb89a7d5239f7fae9fc1
Yzer De Gula <yzerdegula@gmail.com>
2023-07-08 23:00:04 -0400

    Add files via upload

--- MinHeap/MinHeap.h
+++ MinHeap/MinHeap.h
@@ -4,9 +4,8 @@
 #ifndef MINHEAP_H
 #define MINHEAP_H
 
-#include <array>
+#include <vector>
 #include <stdexcept>
-#include <functional>
 #include <iostream>
 
 
@@ -16,13 +15,99 @@ template <typename Comparable>
 class MinHeap
 {
     public:
-        MinHeap() : current_size_(0)
+        // Default Constructor
+        MinHeap() : current_size_(0), array_(MAX_SIZE + 1)
+        {} // End Default Constructor 
+
+
+        // Parameterized Constructor (L-Value)
+        MinHeap(const std::vector<Comparable>& elements)
+        : current_size_(elements.size()), array_(MAX_SIZE + 1)
         {
+            if (current_size_ > MAX_SIZE)
+                throw std::out_of_range("Error: Number of elements exceed Heap size.\n");
+            
+            for (size_t i = 0; i < current_size_; i++)
+                array_[i + 1] = elements[i];
+            
+            heapify();
+        } // End Parameterized Constructor (L-Value)
 
-        }
+
+        // Parameterized Constructor (R-Value)
+        MinHeap(std::vector<Comparable>&& elements)
+        : current_size_(elements.size()), array_(MAX_SIZE + 1)
+        {
+            if (current_size_ > MAX_SIZE)
+                throw std::out_of_range("Error: Number of elements exceed Heap size.\n");
+
+            for (size_t i = 0; i < current_size_; i++)
+                array_[i + 1] = elements[i];
+            
+            heapify();
+        } // End Parameterized Constructor (R-Value)
 
 
-        void insert( const Comparable & new_item )
+        // Copy Constructor
+        MinHeap(const MinHeap &rhs) : current_size_(rhs.current_size_), array_(rhs.array_)
+        {} // End Copy Constructor
+
+        // Copy Assignment
+        MinHeap& operator=(const MinHeap &rhs)
+        {
+            current_size_ = rhs.current_size_;
+            array_ = rhs.array_;
+
+            heapify();
+
+            return *this;
+        } // End Copy Assignment
+
+        
+        // Move Constructor
+        MinHeap(MinHeap &&rhs)
+        {
+            current_size_ = rhs.current_size_;
+            array_ = rhs.array_;
+
+            rhs.current_size_ = 0;
+        } // End Move Constructor
+
+        
+        // Move Assignment
+        MinHeap& operator=(MinHeap &&rhs)
+        {
+            std::swap(current_size_, rhs.current_size_);
+            std::swap(array_, rhs.array_);
+
+            rhs.current_size_ = 0;
+
+            return *this;
+        } // End Move Assignment
+
+
+        // Destructor 
+        ~MinHeap()
+        {
+            current_size_ = 0;
+        } // End Destructor
+
+
+        // Assignment Overload
+        MinHeap& operator=(const std::vector<Comparable> &elements)
+        {
+            current_size_ = elements.size();
+            array_.resize(MAX_SIZE + 1);
+
+            for (size_t i = 0; i < current_size_; i++)
+                array_[i + 1] = elements[i];     
+            heapify();
+
+            return *this;
+        } // End Assignment Overload
+
+
+        void insert(const Comparable &new_item)
         {
             if (isFull())
                 throw std::out_of_range("Heap is full, Unable to insert element.\n");
@@ -39,7 +124,7 @@ class MinHeap
         void deleteMin()
         {
             if (isEmpty())
-                throw std::out_of_range("Heap is empty. No maximum element present.\n");
+                throw std::out_of_range("Heap is empty, No minimum element present.\n");
 
             array_[1] = array_[current_size_];
             current_size_--;
@@ -49,15 +134,13 @@ class MinHeap
         void heapify()
         {
             for (int i = current_size_ / 2; i > 0; i--)
-            {
                 percolateDown(i);
-            }
         }
 
         int getMin() const
         {
             if (isEmpty())
-                throw std::out_of_range("Heap is empty. No maximum element present.\n");
+                throw std::out_of_range("Heap is empty, No minimum element present.\n");
 
             return array_[1];
         }
@@ -69,10 +152,24 @@ class MinHeap
 
         void display() const
         {
-            std::cout << "Array: " << std::endl;
             for (size_t i = 1; i <= current_size_; i++)
             {
-                std::cout << "Array[" << i << "]: " << array_[i] << std::endl; 
+                std::cout << "Index[" << i << "]: " << array_[i];
+
+                size_t left_child_index = 2 * i;
+                size_t right_child_index = 2 * i + 1;
+
+                if (left_child_index <= current_size_)
+                {
+                    std::cout << ", Left Child[" << left_child_index << "]: " << array_[left_child_index];
+                }
+
+                if (right_child_index <= current_size_)
+                {
+                    std::cout << ", Right Child[" << right_child_index << "]: " << array_[right_child_index];
+                }
+
+                std::cout << std::endl;
             }
             std::cout << std::endl;
         }
@@ -80,7 +177,7 @@ class MinHeap
 
     private:
         static const int MAX_SIZE = 100;
-        Comparable array_[MAX_SIZE + 1];
+        std::vector<Comparable> array_;
         size_t current_size_;
 
         void percolateDown( int hole )
@@ -88,7 +185,7 @@ class MinHeap
             int child;
             Comparable temp = array_[hole];
 
-            while (2*hole <= current_size_)
+            while (2 * hole <= current_size_)
             {
                 child = hole * 2;
                 if (child != current_size_ && array_[child + 1] < array_[child])
@@ -113,7 +210,6 @@ class MinHeap
         {
             return current_size_ == MAX_SIZE;
         }
-
 
 };
 
--- MinHeap/main.cc
+++ MinHeap/main.cc
@@ -18,27 +18,71 @@ void testMinHeap()
     heap1.insert(15);
 
 
-    std::cout << "After Inserting 5, 10, and 15 " << std::endl;
+    std::cout << "After Inserting 5, 10, and 15: " << std::endl;
     std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
-    std::cout << "Heap 1 - Minimum Element: " << heap1.getMin() << std::endl;
+    std::cout << "Minimum Element: " << heap1.getMin() << std::endl;
     heap1.display();
 
-    std::cout << "After Popping 5 " << std::endl;
+    std::cout << "After Popping 5: " << std::endl;
     heap1.deleteMin();
     std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
-    std::cout << "Heap 1 - Minimum Element After Deleting: " << heap1.getMin() << std::endl;
+    std::cout << "Minimum Element After Deleting: " << heap1.getMin() << std::endl;
     heap1.display();
 
-    std::cout << "After Popping 10 and 15 (Empty Heap)" << std::endl;
+    std::cout << "After Popping 10 and 15 (Empty Heap):" << std::endl;
     heap1.deleteMin();
     heap1.deleteMin();
     std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
-    std::cout << "Heap 1 - Minimum Element After Deleting: " << heap1.getMin() << std::endl;
+    std::cout << "Minimum Element After Deleting All Elements: " << heap1.getMin() << std::endl;
     heap1.display();
+
+}
+
+void testMinHeap2()
+{
+    // TESTS PARAMETERIZED CONSTRUCTOR (L-VALUE)
+    std::vector<int> elements = {9,5,2,7,1,8};
+    MinHeap<int> heap2(elements);
+    std::cout << "Heap 2: " << std::endl; heap2.display();
+
+    // TESTS COPY CONSTRUCTOR
+    std::cout << "After Copy Constructor heap3{heap2}: " << std::endl;
+    MinHeap<int> heap3(heap2);
+    std::cout << "Heap 3: " << std::endl; heap3.display();
+
+    // TESTS PARAMETERIZED CONSTRUCTOR (R-VALUE) & COPY ASSIGNMENT
+    MinHeap<int> heap4(std::vector<int>{50,60,70,75,100});
+    std::cout << "Heap 4 Before Copy Assignment: " << std::endl;
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+    std::cout << "After heap4 = heap2 " << std::endl;
+    heap4 = heap2;
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+
+    // TESTS MOVE CONSTRUCTOR
+    MinHeap<int> heap5 = std::move(heap4);
+    std::cout << "After heap 5= std::move(heap4) " << std::endl; 
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+
+    // TESTS MOVE ASSIGNMENT
+    MinHeap<int> heap6(std::vector<int>{17,21,16,76});
+    std::cout << "Heap 6: " << std::endl; heap6.display();
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+    std::cout << "After heap5 = std::move(heap6): " << std::endl;
+    heap5 = std::move(heap6);
+    std::cout << "Heap 6: " << std::endl; heap6.display();
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+
+    // TESTS OVERLOADED ASSIGNMENT
+    heap5 = std::vector<int>{69,420};
+    std::cout << "Testing heap5 = std::vector<int>{69,420}" << std::endl;
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+
 }
 
 int main()
 {
     testMinHeap();
+    //testMinHeap2();
     return 0;
 }
