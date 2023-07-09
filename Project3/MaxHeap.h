463239995d16cf52f1c6ed5727f360b956ce529c
Yzer De Gula <yzerdegula@gmail.com>
2023-07-08 23:00:17 -0400

    Add files via upload

--- Project3/MaxHeap.h
+++ Project3/MaxHeap.h
@@ -1,75 +1,191 @@
+// Yzer De Gula
+// MaxHeap.h Template Class for a Max Heap
+
 #ifndef MAXHEAP_H
 #define MAXHEAP_H
 
-#include <array>
+#include <vector>
 #include <stdexcept>
-#include <functional>
-
-#define MAX_SIZE 100
+#include <iostream>
 
 // Max Heap Implementation
 template <typename Comparable>
 class MaxHeap
 {
     public:
-        MaxHeap() : current_size_(0)
-        {
-            for (int i = 0; i <= MAX_SIZE; i++)
-            {
-                array_[i] = Comparable();
-            }      
-        }
+        // Default Constructor
+        MaxHeap() : current_size_(0), array_(MAX_SIZE + 1)
+        {} // End Default Constructor
 
-        MaxHeap(const std::array<Comparable, MAX_SIZE>& elements) : current_size_(elements.size())
+
+        // Parameterized Constructor (L-Value)
+        MaxHeap(const std::vector<Comparable> &elements)
+        : current_size_(elements.size()), array_(MAX_SIZE + 1)
         {
             if (current_size_ > MAX_SIZE)
-            {
-                throw std::out_of_range("Number of elements exceeds the maximum heap size.\n");
-            }
-
-            for (int i = 0; i < current_size_; i++)
-            {
+                throw std::out_of_range("Error: Number of elements exceed Heap size.\n");
+            
+            for (size_t i = 0; i < current_size_; i++)
                 array_[i + 1] = elements[i];
-            }
+            
             heapify();
-        }
+        } // End Parameterized Constructor (L-Value)
+
+        
+        // Parameterized Constructor (R-Value)
+        MaxHeap(std::vector<Comparable> &&elements)
+        : current_size_(elements.size()), array_(MAX_SIZE + 1)
+        {
+            if (current_size_ > MAX_SIZE)
+                throw std::out_of_range("Error: Number of elements exceed Heap size.\n");
+
+            for (size_t i = 0; i < current_size_; i++)
+                array_[i + 1] = elements[i];
+            
+            heapify();
+        } // End Parameterized Constructor (R-Value)
 
 
-        void insert(const Comparable& new_item)
+        // Copy Constructor 
+        MaxHeap(const MaxHeap &rhs) : current_size_(rhs.current_size_), array_(rhs.array_)
+        {} // End Copy Constructor
+
+
+        // Copy Assignment
+        MaxHeap& operator=(const MaxHeap &rhs)
+        {
+            current_size_ = rhs.current_size_;
+            array_ = rhs.array_;
+
+            heapify();
+
+            return *this;
+        } // End Copy Assignment
+
+
+        // Move Constructor
+        MaxHeap(MaxHeap &&rhs)
+        {
+            current_size_ = rhs.current_size_;
+            array_ = rhs.array_;
+
+            rhs.current_size_ = 0;
+        } // End Move Constructor
+
+
+        // Move Assignment
+        MaxHeap& operator=(MaxHeap &&rhs)
+        {
+            std::swap(current_size_, rhs.current_size_);
+            std::swap(array_, rhs.array_);
+
+            rhs.current_size_ = 0;
+
+            return *this;
+        } // End Move Assignment
+
+
+        // Destructor
+        ~MaxHeap()
+        {
+            current_size_ = 0;
+        } // End Destructor
+
+        
+        // Assignment Overload
+        MaxHeap& operator=(const std::vector<Comparable> &elements)
+        {
+            current_size_ = elements.size();
+            array_.resize(MAX_SIZE + 1);
+
+            for (size_t i = 0; i < current_size_; i++)
+                array_[i + 1] = elements[i];
+            heapify();
+
+            return *this;
+        } // End Assignment Overload
+
+
+        // insert
+        void insert(const Comparable &new_item)
         {
             if (isFull())
+                throw std::out_of_range("Heap if full, Unable to insert element.\n");
+            
+            int hole = ++current_size_;
+            while (hole > 1 && new_item > array_[hole/2])
             {
-                throw std::logic_error("Heap is full, Unable to insert element.\n");
+                array_[hole] = array_[hole/2];
+                hole = hole/2;
             }
-
-                int hole = current_size_++;
-                while (hole > 1 && new_item > array_[hole/2])
-                {
-                    array_[hole] = array_[hole/2];
-                    hole = hole/2;
-                }
             array_[hole] = new_item;
-        }
+        } // End insert
 
+
+        // deleteMax
         void deleteMax()
         {
             if (isEmpty())
-            {
-                throw std::logic_error("Heap is empty. No maximum element present.\n");
-            }
+                throw std::out_of_range("Heap is empty. No maximum element present.\n");
 
             array_[1] = array_[current_size_];
             current_size_--;
             percolateDown(1);
-        }
+        } // End deleteMax
 
+
+        // heapify
         void heapify()
         {
-            for (int i = current_size_/2; i > 0; i--)
+            for (int i = current_size_ / 2; i > 0; i--)
                 percolateDown(i);
-        }
+        } // End heapify
 
-        void percolateDown(int hole)
+
+        // getMax
+        int getMax() const
+        {
+            if (isEmpty())
+                throw std::out_of_range("Heap is empty, No maximum element present.\n");
+            
+            return array_[1];
+        } // End getMax
+
+        
+        // getCurrentSize
+        int getCurrentSize() const
+        {
+            return current_size_;
+        } // End getCurrentSize
+
+
+        // display
+        void display() const
+        {
+            for (size_t i = 1; i <= current_size_; i++)
+            {
+                std::cout << "Index[" << i << "]: " << array_[i];
+
+                size_t left_child_index = 2 * i;
+                size_t right_child_index = 2 * i + 1;
+
+                if (left_child_index <= current_size_)
+                    std::cout << ", Left Child[" << left_child_index << "]: " << array_[left_child_index];
+
+                if (right_child_index <= current_size_)
+                    std::cout << ", Right Child[" << right_child_index << "]: " << array_[right_child_index];
+
+                std::cout << std::endl;
+            }
+            std::cout << std::endl;
+        } // End display
+
+    private:
+        static const int MAX_SIZE = 100;
+        std::vector<Comparable> array_;
+        size_t current_size_;
+
+        void percolateDown( int hole )
         {
             int child;
             Comparable temp = array_[hole];
@@ -84,45 +200,21 @@ class MaxHeap
                     array_[hole] = array_[child];
                 else
                     break;
-
+                
                 hole = child;
             }
             array_[hole] = temp;
+        } 
+
+        bool isEmpty() const
+        {
+            return current_size_ == 0;
         }
 
         bool isFull() const
         {
-            return (current_size_ == MAX_SIZE);
+            return current_size_ == MAX_SIZE;
         }
-
-        bool isEmpty() const
-        {
-            return (current_size_ == 0);
-        }
-
-        Comparable getMax() const
-        {
-            if (isEmpty())
-            {
-                throw std::logic_error("Heap is empty. No maximum element present.\n");
-            }
-
-            return array_[1];
-        }
-
-        int getCurrentSize() const
-        {
-            return current_size_;
-        }
-
-        std::array<Comparable, MAX_SIZE + 1> getArray() const
-        {
-            return array_;
-        }
-
-    private:
-        std::array<Comparable, MAX_SIZE + 1> array_;
-        int current_size_;
 
 };
 
--- Project3/main.cc
+++ Project3/main.cc
@@ -7,27 +7,83 @@
 
 void testMaxHeap()
 {
+    
     MaxHeap<int> heap1;
+    std::cout << "Empty Heap: " << std::endl;
+    heap1.display();
 
 
-    const auto& arr_1 = heap1.getArray();
-    int size_1 = heap1.getCurrentSize();
+    heap1.insert(5);
+    heap1.display();
+    heap1.insert(10);
+    heap1.insert(15);
 
-    for (int i = 0; i <= size_1; i++)
-    {
-        std::cout << "Array[" << i << "]: " << arr_1[i] << std::endl;
-    }
-    std::cout << "Current Size: " << size_1 << std::endl;
 
-    std::cout << "Heap 1 - Maximum Element: " << heap1.getMax() << std::endl;
+    std::cout << "After Inserting 5, 10, and 15: " << std::endl;
+    std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
+    std::cout << "Maximum Element: " << heap1.getMax() << std::endl;
+    heap1.display();
+
+    std::cout << "After Popping 5: " << std::endl;
+    heap1.deleteMax();
+    std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
+    std::cout << "Maximum Element After Deleting: " << heap1.getMax() << std::endl;
+    heap1.display();
+
+    std::cout << "After Popping 10 and 15 (Empty Heap):" << std::endl;
+    heap1.deleteMax();
+    heap1.deleteMax();
+    std::cout << "Current Size: " << heap1.getCurrentSize() << std::endl;
+    std::cout << "Maximum Element After Deleting All Elements: " << heap1.getMax() << std::endl;
+    heap1.display();
+
+}
+
+void testMaxHeap2()
+{
+    // TESTS PARAMETERIZED CONSTRUCTOR (L-VALUE)
+    std::vector<int> elements = {9,5,2,7,1,8};
+    MaxHeap<int> heap2(elements);
+    std::cout << "Heap 2: " << std::endl; heap2.display();
+
+    // TESTS COPY CONSTRUCTOR
+    std::cout << "After Copy Constructor heap3{heap2}: " << std::endl;
+    MaxHeap<int> heap3(heap2);
+    std::cout << "Heap 3: " << std::endl; heap3.display();
+
+    // TESTS PARAMETERIZED CONSTRUCTOR (R-VALUE) & COPY ASSIGNMENT
+    MaxHeap<int> heap4(std::vector<int>{50,60,70,75,100});
+    std::cout << "Heap 4 Before Copy Assignment: " << std::endl;
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+    std::cout << "After heap4 = heap2 " << std::endl;
+    heap4 = heap2;
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+
+    // TESTS MOVE CONSTRUCTOR
+    MaxHeap<int> heap5 = std::move(heap4);
+    std::cout << "After heap 5= std::move(heap4) " << std::endl; 
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+    std::cout << "Heap 4: " << std::endl; heap4.display();
+
+    // TESTS MOVE ASSIGNMENT
+    MaxHeap<int> heap6(std::vector<int>{17,21,16,76});
+    std::cout << "Heap 6: " << std::endl; heap6.display();
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+    std::cout << "After heap5 = std::move(heap6): " << std::endl;
+    heap5 = std::move(heap6);
+    std::cout << "Heap 6: " << std::endl; heap6.display();
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+
+    // TESTS OVERLOADED ASSIGNMENT
+    heap5 = std::vector<int>{69,420};
+    std::cout << "Testing heap5 = std::vector<int>{69,420}" << std::endl;
+    std::cout << "Heap 5: " << std::endl; heap5.display();
+
 }
 
 int main()
 {
-    testMaxHeap();
+    //testMaxHeap();
+    testMaxHeap2();
     return 0;
 }
-
-/*
-    Make Constructors, Destructor, and solve Private Var situation, Max Heap with the arrays with the + 1 or the vector
-*/
